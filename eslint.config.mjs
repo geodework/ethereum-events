26c7fbf16184b519716151f3905695a15f59eb4b
toshiboy86 <toshibowie86@gmail.com>
2025-05-08 16:19:37 +0900

    feat: set eslint config with local vscode

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,82 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import unusedImports from "eslint-plugin-unused-imports"
+import { FlatCompat } from "@eslint/eslintrc"
+import typescriptEslint from "@typescript-eslint/eslint-plugin"
+import typescriptParser from "@typescript-eslint/parser"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+  recommendedConfig: {},
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "prettier"
+  ),
+  {
+    ignores: [".next/**/*"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      parser: typescriptParser,
+      globals: {
+        // Add any global variables here
+      },
+    },
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      "@typescript-eslint": typescriptEslint,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            [
+              "^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)",
+            ],
+            ["^react(?!-.)$", "^\\w", "^@\\w"],
+            ["^@/lib/types", "^@/lib/interfaces"],
+            ["^@/components"],
+            ["^@/lib/utils"],
+            ["^@/data"],
+            ["^@/lib/constants"],
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+            ["^.+\\.s?css$"],
+            ["^\\u0000"],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": [
+        "error",
+        {
+          fixToUnknown: true,
+        },
+      ],
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
