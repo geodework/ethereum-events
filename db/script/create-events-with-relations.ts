6f3a792bd189863533a0b63ec5904198efffbcdd
toshiboy86 <toshibowie86@gmail.com>
2025-05-13 16:41:35 +0900

    feat: insert events and relevant manyToMany data in bridge tables

---
+++ db/script/create-events-with-relations.ts
@@ -0,0 +1,60 @@
+import { db } from '../index';
+import eventsData from '../output/events.json';
+import { eventCategoryEvents, eventDomainEvents, events } from '../schema';
+
+const main = async () => {
+  if (eventsData?.length === 0) {
+    throw new Error(
+      'No events data found in output/events.json. Generate it first.'
+    );
+  }
+
+  console.log('events processing starts...');
+  const eventsPayload = eventsData.map((e) => {
+    const { categories, domains, ...rest } = e;
+
+    return {
+      ...rest,
+      startDateTime: rest.startDateTime ? new Date(rest.startDateTime) : null,
+      endDateTime: rest.endDateTime ? new Date(rest.endDateTime) : null,
+    };
+  });
+  console.log('events payload created');
+  const eventsResult = await db
+    .insert(events)
+    .values(eventsPayload)
+    .returning();
+  console.log('events inserted', eventsResult);
+
+  if (eventsResult.length !== eventsPayload.length) {
+    throw new Error('Failed to insert events');
+  }
+
+  const eventsToCategoryPayload: { eventId: number; categoryId: number }[] = [];
+  const eventsToDomainPayload: { eventId: number; domainId: number }[] = [];
+
+  console.log('bridging data creating...');
+  eventsResult.forEach((result: { id: number }, idx) => {
+    eventsData[idx].categories.forEach((categoryId) => {
+      eventsToCategoryPayload.push({
+        eventId: result.id,
+        categoryId: categoryId,
+      });
+    });
+    eventsData[idx].domains.forEach((domainId) => {
+      eventsToDomainPayload.push({
+        eventId: result.id,
+        domainId: domainId,
+      });
+    });
+  });
+  console.log('bridging data created');
+  await db.insert(eventCategoryEvents).values(eventsToCategoryPayload);
+  console.log('eventCategoryEvents inserted');
+  await db.insert(eventDomainEvents).values(eventsToDomainPayload);
+  console.log('eventDomainEvents inserted');
+
+  console.log('Database updated');
+};
+
+main().catch(console.error);
