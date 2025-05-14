5d7e1c7abfffff93e464247f78ae9d768c1cee4a
toshiboy86 <toshibowie86@gmail.com>
2025-05-14 14:58:39 +0900

    docs: add note for supabase bug for policy settings

--- db/policy.ts
+++ db/policy.ts
@@ -1,6 +1,8 @@
 import { sql } from 'drizzle-orm';
 import { pgPolicy } from 'drizzle-orm/pg-core';
 
+// Note: There is a bug in supabase that doesn't allow to use sql`true`.
+// I manually added the policy to enable read access to all users in Supabase dashboard.
 const policies = [
   pgPolicy('Enable read access to all users', {
     as: 'permissive',
