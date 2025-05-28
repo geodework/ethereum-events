f966466d37927de576b9364c0bb2b5cd96a66ba8
toshiboy86 <toshibowie86@gmail.com>
2025-05-28 11:27:42 +0900

    feat: add getRegions function to retrieve continent data from Supabase

---
+++ repositories/regions.ts
@@ -0,0 +1,9 @@
+import supabase from "../supabase"
+
+export const getRegions = async () => {
+  const { data, error } = await supabase.from("continents").select("*")
+  if (error) {
+    throw error
+  }
+  return data
+}
