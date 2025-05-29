def8525d2c667a183b91635a59978c98918761bf
toshiboy86 <toshibowie86@gmail.com>
2025-05-29 16:11:16 +0900

    feat: add categories repository

---
+++ repositories/categories.ts
@@ -0,0 +1,9 @@
+import supabase from "../supabase"
+
+export const getCategories = async () => {
+  const { data, error } = await supabase.from("event_categories").select("*")
+  if (error) {
+    throw error
+  }
+  return data
+}
