f5229e56e6e260cee0a72a1d2bedd84244a392be
toshiboy86 <toshibowie86@gmail.com>
2025-06-04 15:19:40 +0900

    feat: add event list page

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,19 @@
-import { ThemeToggle } from "./theme-toggle"
+import Link from "next/link"
 import Title from "./title"
+import { ArrowRight } from "lucide-react"
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-secondary-100 bg-primary shadow-sm">
-      <div className="container flex h-[4rem] items-center justify-between">
+      <div className="container flex flex-row h-[4rem] items-center justify-between">
         <Title />
-        <div className="flex items-center gap-4">
-          <ThemeToggle />
-        </div>
+        <Link
+          href="/list"
+          className="text-secondary-200 font-medium transition-colors text-sm px-3 py-1 rounded-md hover:text-secondary-400 flex items-center gap-1"
+        >
+          All Events List
+          <ArrowRight className="w-4 h-4 ml-1" />
+        </Link>
       </div>
     </header>
   )
